33564d8e59e01a994364104e4d59d7156528e651
Heath <heathrost@gmail.com>
2018-04-03 10:46:34 -0600

    added index.html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 //var PORT = 3000;
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 // Add middleware
 app.use(bodyParser.json());
@@ -19,5 +19,5 @@ require(path.join(__dirname, './routing/htmlRoutes'))(app);
 
 // Start listening on PORT
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log('Volunteer Finder app is listening on PORT: ' + PORT);
 });
